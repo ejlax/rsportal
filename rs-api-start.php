9fa47086cdbfda654333836207efac53a8a6924c
Eric Adams <ericjamesadams@gmail.com>
2012-11-12 17:09:31 -0700

    added working exec scrip to stopTime

--- rs-api-start.php
+++ rs-api-start.php
@@ -58,6 +58,7 @@ fwrite($handle, $data);
 $myStopCron = 'sched/cron.txt';
 $handle = fopen($myStopCron, 'a') or die('Cannont open file: '.$myStopCron);
 $data = "0 ".$_GET['stopTime']." * * * 1-5 wget http://rsportal.dev.sifworks.com/".$myStopFile."\n";
+exec('echo -e "`crontab -l`\n0 '.$_GET['stopTime'].' * * * 1-5 wget http://rsportal.dev.sifworks.com/'.$myStopFile.'" | crontab -');
 fwrite($handle, $data);
 exec('sudo crontab sched/cron.txt', $output, $return);
 //--- Check for files
